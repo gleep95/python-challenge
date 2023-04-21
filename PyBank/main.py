a9462c69940942b3596e44ecf4db6e3752070409
gpanchy <gpanchy@gmail.com>
2023-04-20 23:06:25 -0500

    Updating files

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,12 +6,14 @@ import csv
 budgetdata_csv = os.path.join("Resources", "budget_data.csv")
 
 #Works cited: https://codecaliper.me/counting-the-number-of-rows-in-a-csv-file-with-python
+#Open budget data, skip header and set variables/counters/empty listlists
 with open(budgetdata_csv,"r") as csv_reader:
     line = next(csv_reader)
     csv_reader = csv.reader(csv_reader)
-    row_count = 0
+    row_count = 1
     total_pnl = 0
     line = next(csv_reader)
+    line1 = int(line[1])
     previous_pnl = int(line[1])
     all_pnl_differences = []
     greatest_pnl_inc = 0
@@ -19,16 +21,20 @@ with open(budgetdata_csv,"r") as csv_reader:
     greatest_pnl_dec = 0
     greatest_dec_date = 0
     
-
+    #Loop over every row in data file and count the total months, and sum of profit/loss column, as well as average change
+    #Works Cited: https://codecaliper.me/counting-the-number-of-rows-in-a-csv-file-with-python
+    #Works Cited: https://stackoverflow.com/questions/13517080/sum-a-csv-column-in-python
     for row in csv_reader:
         row_count += 1
         total_pnl += int(row[1])
         date = str(row[0])
         pnl = int(row[1])  
         pnl_difference = pnl - previous_pnl
+        #Get the greatest increase in profits and date
         if greatest_pnl_inc < pnl_difference:
             greatest_pnl_inc = pnl_difference 
             greatest_inc_date = date
+        #Get the greatest decrease in profits and date
         elif greatest_pnl_dec > pnl_difference:
             greatest_pnl_dec = pnl_difference
             greatest_dec_date = date
@@ -36,7 +42,8 @@ with open(budgetdata_csv,"r") as csv_reader:
         previous_pnl = pnl
 
     
-
+#Print out the results of months, sum of P/L column, total average change, greatest inc in profits with date, greatest dec in profits with date
+#Works Cited: https://www.adamsmith.haus/python/answers/how-to-format-currency-in-python
 print()
 print("Financial Analysis")
 print()
@@ -44,7 +51,7 @@ print("---------------------------------------")
 print()
 print("Total Months: ", row_count)  
 print()
-print("Total: " "${:,.2f}".format(total_pnl)) 
+print("Total: " "${:,.2f}".format(total_pnl + line1)) 
 print()
 print("Average Change: ", "${:,.2f}".format(round(sum(all_pnl_differences)/len(all_pnl_differences),2)))
 print()
@@ -53,12 +60,13 @@ print()
 print("Greatest Decrease in Profits: ", greatest_dec_date, "${:,.2f}".format(greatest_pnl_dec))
 print()
 
-
+#Write out the results of months, sum of P/L column, total average change, greatest inc in profits with date, greatest dec in profits with date to results text file
+#Works Cited: https://www.adamsmith.haus/python/answers/how-to-format-currency-in-python
 txt_file = open("analysis/results_file.txt", "w")
 txt_file.write("Financial Analysis\n")
 txt_file.write("---------------------------------------\n")
 txt_file.write("Total Months: "+ str(row_count)+"\n")  
-total_pnl = "Total: " "${:,.2f}".format(total_pnl) + "\n"
+total_pnl = "Total: " "${:,.2f}".format(total_pnl+line1) + "\n"
 txt_file.write(total_pnl) 
 avg_change = "Average Change: "+ "${:,.2f}".format(round(sum(all_pnl_differences)/len(all_pnl_differences),2))
 greatest_inc = "Greatest Increase in Profits: "+ greatest_inc_date+ " ${:,.2f}".format(greatest_pnl_inc)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,23 @@
 import pandas as pd
 
-
+#Open and read election poll data
+#Works Cited: https://www.geeksforgeeks.org/python-read-csv-using-pandas-read_csv/
 df = pd.read_csv("C:/Users/gpanc/OneDrive/Documents/python-challenge/PyPoll/Resources/election_data.csv")
+
+#Create a list of the unique names of the candidates, then the whole list of candidates
+#Works Cited: https://stackoverflow.com/questions/71098589/pandas-return-column-data-as-list-without-duplicates
 candidate_names = list(df["Candidate"].unique())
 candidates = (df["Candidate"])
+
+#Set variables/counters
 percent1 = 0
 greatest_count_votes = 0
 winner = 0
+
+#Get the total votes
 total = len(df)
 
+#Print the Election Results statement and total votes
 print()
 print("Election Results")
 print()
@@ -19,7 +28,8 @@ print()
 print("--------------------------------")
 print()
 
-
+#Write the Election Results statement and total votes to a results text file
+#Works Cited: https://blog.finxter.com/how-to-print-a-percentage-value-in-python/
 txt_file = open("C:/Users/gpanc/OneDrive/Documents/python-challenge/PyPoll/analysis/results_file.txt", "w")
 txt_file.write("Election Results\n")
 line1 = ("---------------------------------------\n")
@@ -31,18 +41,26 @@ txt_file.write(line2)
 
 
 
-
+#Loop through names of unique list and set a counter
 for name in candidate_names:
     count_names = 0
+    
+    #Loop through whole candidates list looking for the unique names and counting them
     for candidate in candidates:
         if name == candidate:
             count_names +=1 
+    
+    #Find the names that has the greatest amount of votes
     if(greatest_count_votes < count_names):
         greatest_count_votes = count_names
         winner = name    
+    
+    #Print the results of the candidate name, percentage of votes and total votes
     print(name, "{:.3%}".format(count_names/len(df)), count_names)
 
-
+    #Write the results of the candidate name, percentage of votes and total votes to results text file
+    #As well as writing the winner
+    #Works Cited: #Works Cited: https://blog.finxter.com/how-to-print-a-percentage-value-in-python/
     candidate_lists = name + " " +"{:.3%}".format(count_names/len(df))+ " "+ str(count_names)+ "\n"
     txt_file.write(candidate_lists)
 line3 = ("---------------------------------------\n")
@@ -50,12 +68,9 @@ txt_file.write(line3)
 txt_file.write("Winner: " + winner+ "\n")
 line4 = ("---------------------------------------\n")
 txt_file.write(line4)
-
 txt_file.close()
 
-
-
-
+#Print out the winner results
 print()
 print("--------------------------------")
 print()
